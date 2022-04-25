6a91a72e612eb23945e7cca8c236faec34124d50
kumo829 <amonmar2000@hotmail.com>
2022-04-25 22:39:47 +0900

    ➕ADD: Last part of the second Chapter for OO

--- src/main/scala/com/tutorials/scala/lightspeed/ObjectOrientation.scala
+++ src/main/scala/com/tutorials/scala/lightspeed/ObjectOrientation.scala
@@ -14,6 +14,9 @@ object ObjectOrientation extends App {
   val animal1: Animal = new Animal
   val animal2 = new Animal
 
+  println(animal1 == animal1)
+  println(animal1 == animal2)
+
   //Inheritance
   class Dog(val name: String) extends Animal //Constructor definition. Constructor arguments are NOT fields: Need to put 'val' before the constructor argument
 
@@ -52,4 +55,77 @@ object ObjectOrientation extends App {
   println(basicMath)
   println(anotherBasicMath)
 
+  //singleton object
+  object MySingleton { //the only instance of the MySingleton type
+    val mySpecialValue = 53278
+
+    def mySpecialMethod() : Int = 53278
+
+    def apply(x: Int): Int = x + 1
+  }
+
+  println(MySingleton.mySpecialMethod())
+  println(MySingleton.apply(10))
+  println(MySingleton(10)) //equivalent to MySingleton.apply(10)
+
+
+  object Animal { //companion object
+    //companions can access each other's private fields/methods
+    //Singleton Animal and instances of Animal class are different things
+    val liveForever = false
+  }
+  println(Animal.liveForever) //call to static
+  println(Animal.getClass.==(animal1.getClass))
+  println(animal2.getClass.==(animal1.getClass))
+  println(Animal.getClass)
+  println(animal1.getClass)
+
+
+  /* case classes = lightweight data structures with some boilerplate
+    - sensible equals and hash code
+    - serialization
+    - companion with apply
+    - pattern matching
+  */
+  case class Person(name: String, age: Int)
+
+  //May be constructed without new
+  val bob = Person("Bob", 10) //Person.apply("Bob", 54)
+
+  println(bob)
+
+
+  //exceptions
+  try {
+    //code that can throw
+    val x: String = null
+    x.length
+  } catch {
+    case e: Exception => "some faulty error message"
+  } finally {
+    //execute some code no matter what
+  }
+
+
+  //generics
+  abstract class MyList[T]{
+    def head: T
+    def tail: MyList[T]
+  }
+
+  //using a generic with a concrete type
+  val aList: List[Int] = List(1,2,3) //List.apply(1,2,3)
+  val first = aList.head
+  val rest = aList.tail
+  val aStringList = List("Hello", "Scala")
+  val firstString = aStringList.head
+
+
+//  Point #1: In Scala we usually operate with IMMUTABLE values/objects
+//  any modification to an object must return ANOTHER object
+  /*
+  Benefits:
+    - Works miracles in multithreading/dist environments
+   */
+  val reversedList = aList.reverse //returns a NEW List
 }
